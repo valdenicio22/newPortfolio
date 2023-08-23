60779fe76eafbfa217e01ccef8d6ddb8cbe8b35b
valdenicio22 <valdenicio22@gmail.com>
2023-08-23 12:16:58 +0200

    feat: add clsx and twMerge libs

--- src/components/Header/NavItem.tsx
+++ src/components/Header/NavItem.tsx
@@ -1,13 +1,22 @@
-import Link from "next/link";
+import { cn } from '@/utils/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 interface NavItemProps {
-  href: string;
+  href: string
   label: string
 }
 
 export function NavItem({ href, label }: NavItemProps) {
-  return <Link href={href} className={`flex items-center gap-2 text-gray-400 font-mono font-medium`}>
-    <span className={`text-emerald-400`}>#</span>
-    {label}
-  </Link>
+  const currentPath = usePathname()
+  const isActive = currentPath === href
+  return (
+    <Link
+      href={href}
+      className={cn(`flex items-center gap-2 text-gray-400 font-mono font-medium text-sm`, isActive && 'text-gray-50')}
+    >
+      <span className={`text-emerald-400`}>#</span>
+      {label}
+    </Link>
+  )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-import { NavItem } from "./NavItem";
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { NavItem } from './NavItem'
 
 const NAV_ITEMS = [
   {
@@ -14,14 +16,23 @@ const NAV_ITEMS = [
 ]
 
 export function Header() {
-  return <header className='flex items-center justify-center absolute top-0 z-10 w-full h-24'>
-    <div className='container flex items-center justify-between'>
-      <Link href='/'>
-        <Image width={58} height={49} src='images/logo.svg' alt='Placeholder Logo' />
-      </Link>
-      <nav className="flex items-center gap-4 sm:gap-10">
-        {NAV_ITEMS.map((item) => <NavItem key={item.label} {...item} />)}
-      </nav>
-    </div>
-  </header>
+  return (
+    <header className="flex items-center justify-center absolute top-0 z-10 w-full h-24">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <Image
+            width={58}
+            height={49}
+            src="images/logo.svg"
+            alt="Placeholder Logo"
+          />
+        </Link>
+        <nav className="flex items-center gap-4 sm:gap-10">
+          {NAV_ITEMS.map((item) => (
+            <NavItem key={item.label} {...item} />
+          ))}
+        </nav>
+      </div>
+    </header>
+  )
 }
