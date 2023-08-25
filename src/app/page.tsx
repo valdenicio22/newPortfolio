596b8784f74f873bf3af7717acd26d9a80df18e9
valdenicio22 <valdenicio22@gmail.com>
2023-08-25 15:08:09 +0200

    feat: create knowledge section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Competence } from "@/components/pages/home/Competence";
 import { HeroSection } from "../components/pages/home/HeroSection";
 
 export default function Home() {
-  return <HeroSection />
+  return <>
+    <HeroSection />
+    <Competence />
+  </>
 }
