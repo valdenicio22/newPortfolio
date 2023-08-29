0a86d0c925da27be02c2c44124fa09cee78099fc
valdenicio22 <valdenicio22@gmail.com>
2023-08-29 14:44:40 +0200

    feat: create  experience section

---
+++ src/components/pages/home/Experience/index.tsx
@@ -0,0 +1,23 @@
+import { SectionTitle } from "@/components/SectionTitle";
+import { KnowledgeBox } from "@/components/pages/home/Experience/knowledgeBox";
+import { TbBrandNextjs } from "react-icons/tb";
+
+
+export function Experience() {
+  return <section className='container py-16'>
+    <SectionTitle section="Compotências" title="Conhecimentos" />
+    <div className={'grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 mt-[60px]'}>
+      {Array.from({ length: 8 }).map((_, i) => (
+
+        <KnowledgeBox key={i} tech={{
+          name: 'NextJs',
+          icon: <TbBrandNextjs />,
+          startDate: '2022-01-01'
+        }} />
+
+      ))}
+    </div>
+
+  </section>
+
+}
---
+++ src/components/pages/home/Experience/knowledgeBox/index.tsx
@@ -0,0 +1,25 @@
+import { getRelativeTimeString } from "@/utils/getRelativeTime";
+import { ReactNode } from "react";
+
+interface KnowlodgeBoxProps {
+  tech: {
+    icon: ReactNode;
+    name: string;
+    startDate: string;
+  }
+}
+
+export function KnowledgeBox({ tech }: KnowlodgeBoxProps) {
+  const { icon, name, startDate } = tech
+  const relativeTime = getRelativeTimeString(
+    new Date(startDate), 'en').replace('ago', '');
+  return (
+    <div className='max-w-[264px] p-6 rounded-lg text-gray-500 flex flex-col gap-2 bg-gray-600/20 hover:text-emerald-500 hover:bg-gray-600/30 transition-all'>
+      <div className='flex items-center justify-between'>
+        <p className='font-medium'>{name}</p>
+        {icon}
+      </div>
+      <span>{`${relativeTime} of experience`}</span>
+    </div>
+  )
+}
